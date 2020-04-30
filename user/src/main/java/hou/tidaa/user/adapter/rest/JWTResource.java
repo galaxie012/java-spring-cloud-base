ca83db0b57677eda3521bdd8c1eef753f442a03e
tida <496164247@qq.com>
2020-04-30 22:18:58 +0800

    登录签发token

--- user/src/main/java/hou/tidaa/user/adapter/rest/JWTResource.java
+++ user/src/main/java/hou/tidaa/user/adapter/rest/JWTResource.java
@@ -5,9 +5,13 @@ import hou.tidaa.core.security.token.Token;
 import hou.tidaa.core.security.token.TokenSignService;
 import hou.tidaa.user.domain.dto.UsernamePasswordDto;
 import hou.tidaa.user.domain.ports.UserAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
@@ -21,11 +25,14 @@ public class JWTResource {
     private final UserAuthentication userAuthentication;
     private final TokenSignService<String> tokenSignService;
 
+    @Autowired
     public JWTResource(final UserAuthentication userAuthentication, final TokenSignService<String> tokenSignService) {
         this.userAuthentication = userAuthentication;
         this.tokenSignService = tokenSignService;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Token<String>> login(@Valid @RequestBody final UsernamePasswordDto dto)
             throws AuthenticationException {
         final IdentityDto auth = userAuthentication.authentication(dto)
