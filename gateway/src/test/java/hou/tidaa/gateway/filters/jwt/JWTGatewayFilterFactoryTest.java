e4e25818e1f1156ea52c6d3811f0e1f23f1ecdd3
tida <496164247@qq.com>
2020-04-30 17:21:42 +0800

    ""

--- gateway/src/test/java/hou/tidaa/gateway/filters/jwt/JWTGatewayFilterFactoryTest.java
+++ gateway/src/test/java/hou/tidaa/gateway/filters/jwt/JWTGatewayFilterFactoryTest.java
@@ -9,7 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMessage;
 
+import java.util.ArrayList;
+
+import static hou.tidaa.gateway.filters.jwt.JWTException.AUTHORIZATION_CONTENT_IS_MISSING;
+import static hou.tidaa.gateway.filters.jwt.JWTException.AUTHORIZATION_HEADER_IS_MISSING;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @ExtendWith(MockitoExtension.class)
 public class JWTGatewayFilterFactoryTest {
@@ -27,5 +33,22 @@ public class JWTGatewayFilterFactoryTest {
         Exception exception = assertThrows(JWTException.class, () ->{
             jwtFilter.extractJWTToken(httpMessage);
         });
+
+        assertEquals(AUTHORIZATION_HEADER_IS_MISSING, exception.getMessage());
+    }
+
+    @Test
+    void authorization_content_is_missing() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.put(AUTHORIZATION, new ArrayList<>());
+
+        Mockito.when(httpMessage.getHeaders())
+                .thenReturn(httpHeaders);
+
+        Exception exception = assertThrows(JWTException.class, () -> {
+            jwtFilter.extractJWTToken(httpMessage);
+        });
+
+        assertEquals(AUTHORIZATION_CONTENT_IS_MISSING, exception.getMessage());
     }
 }
